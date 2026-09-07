f7c3158e328af95b3dd0b7c6bf2b65c404502d97
lauferreyra <lauferreyra10@gmail.com>
2026-09-07 02:11:49 -0300

    feat: add conversational agent memory

--- app/agent.py
+++ app/agent.py
@@ -3,7 +3,8 @@ from langchain.agents import create_agent
 from app.config import settings
 from app.tools import get_available_appointments
 from langchain_ollama import ChatOllama
-
+from datetime import date
+from langchain_core.messages import HumanMessage, AIMessage
 
 llm = ChatOllama(
     model=settings.llm_model,
@@ -14,16 +15,44 @@ agent = create_agent(
     tools=[get_available_appointments],
 )
 
+conversation_history = []
+
+def get_current_date() -> str:
+    return date.today().isoformat()
+
 def run_agent(message: str):
+    current_date = get_current_date()
+
+    conversation_history.append(
+        HumanMessage(content=message)
+    )
+
+    messages = [
+        {
+            "role": "system",
+            "content": (
+                f"La fecha actual es {current_date}. "
+                "Cuando el usuario utilice fechas relativas "
+                "como hoy, mañana o pasado mañana, "
+                "utilizá esta fecha como referencia. "
+                "No inventes años."
+            ),
+        },
+        *conversation_history,
+    ]
+
+    print("HISTORIAL:")
+    print(conversation_history)
     result = agent.invoke(
         {
-            "messages": [
-                {
-                    "role": "user",
-                    "content": message,
-                }
-            ]
+            "messages": messages,
         }
     )
 
-    return result["messages"][-1].content
+    final_message = result["messages"][-1]
+
+    conversation_history.append(
+        AIMessage(content=final_message.content)
+    )
+
+    return final_message.content
