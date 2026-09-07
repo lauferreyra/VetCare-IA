6190106cc964c287096b0277cb8009199cde67da
lauferreyra <lauferreyra10@gmail.com>
2026-09-06 21:42:29 -0300

    feat: add chat endpoint

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from app.schemas import ChatRequest
 
 app = FastAPI()
-
 
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
+
+@app.post("/chat")
+def chat(request: ChatRequest):
+    return {
+        "message": f"Recibí tu mensaje: {request.message}"
+    }
